e74c280e0bc2b29d12d248ffee39b1a6a2ecb7b6
Anoob Bava <anoob.bava@gmail.com>
2020-02-18 15:37:07 +0530

    Completed the basic functionalities

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 import Quote from '../../containers/Quote/Quote';
+import BackgroundImage1 from '../../assets/images/background.jpg';
+
 const Layout = () => {
   return (
     <Aux >
+      <div style={{ backgroundImage: `url(${BackgroundImage1})` }}></div>
       <Quote />
     </Aux >
   )
---
+++ src/components/QuoteText/QuoteText.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const quoteText = (props) => {
+
+  const quoteTextStyle = {
+    'fontSize': '35px',
+    'textAlign': 'center',
+    'padding': '100px',
+  }
+  return (
+    <div style={quoteTextStyle}>
+      <p>{props.quoteText}</p>
+    </div>
+  )
+}
+
+export default quoteText;
