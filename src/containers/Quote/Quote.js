e47ba720aa314089c51fa2f38dee3f190e669228
Anoob Bava <anoob.bava@gmail.com>
2020-02-18 13:30:40 +0530

    Added state and conditional loading

--- src/containers/Quote/Quote.js
+++ src/containers/Quote/Quote.js
@@ -4,11 +4,24 @@ import QuoteText from '../../components/QuoteText/QuoteText';
 import QuoteAuthor from '../../components/QuoteAuthor/QuoteAuthor';
 
 export default class Quote extends Component {
+
+  state = {
+    isLoading: false,
+    quoteText: 'Be yourself; everyone else is already taken',
+    quoteAuthor: 'Oscar Wilde',
+    error: ''
+  }
+
   render() {
+    const { quoteAuthor, quoteText, isLoading } = this.state;
     return (
       <div>
-        <QuoteText quoteText={'Be yourself; everyone else is already taken'} />
-        <QuoteAuthor quoteAuthor={'Oscar Wilde'} />
+        {
+          isLoading ?
+            <div> Loading the data</div>
+            : <div>  <QuoteText quoteText={quoteText} />
+              <QuoteAuthor quoteAuthor={quoteAuthor} /> </div>
+        }
       </div>
     )
   }
