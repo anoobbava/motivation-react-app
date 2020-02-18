d1d08d41ce4b1a4ce0c450435db70f1347a85a28
Anoob Bava <anoob.bava@gmail.com>
2020-02-18 13:02:46 +0530

    Layout Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import './App.css';
+import Layout from './components/Layout/Layout'
 
 function App() {
   return (
     <div className="App">
+      <Layout />
     </div>
   );
 }
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div>
+      <p>this the layout of the Page</p>
+    </div>
+  )
+}
+
+export default Layout;
